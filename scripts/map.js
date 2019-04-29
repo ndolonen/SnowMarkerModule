4b6a51b5d556d1b81aa4349bdabd4997e3c327f5
Njaal <kongnjaal@gmail.com>
2019-04-29 15:00:43 +0200

    Added License

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2019, Njaal Dolonen, Nicolay Skjelbred, Jan-Magnus Solheim All rights reserved.
+ * See LICENSE for more detail.  
+ * */ 
+
 //map imports
 const TileLayer = ol.layer.Tile
 const OSM = ol.source.OSM
--- scripts/style.js
+++ scripts/style.js
@@ -1,4 +1,9 @@
 /**
+ * Copyright (c) 2019, Njaal Dolonen, Nicolay Skjelbred, Jan-Magnus Solheim All rights reserved.
+ * See LICENSE for more detail.  
+ * */ 
+
+/**
  * Script to manage predefined styles.
  */
 
